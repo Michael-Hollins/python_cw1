5791ce40f438bf06a0eac7f6cd774558c79ba941
Michael Hollins <mdh323@ic.ac.uk>
2023-10-11 16:33:03 +0100

    Working version of the code. Need to run more tests

--- bucket_fill.py
+++ bucket_fill.py
@@ -80,6 +80,135 @@ def show_image(image):
     print(stringify_image(image))
 
 
+def validate_seed_point(image, seed_point):
+    """ Check that the starting seed given is valid. If so, do nothing. If not, return the image.
+    
+    Invalid starting seeds would be out of the grid range, non-integer values and
+    starting on a boundary value. This function is a series of logical tests. If 
+    all pass, the function does nothing. If there's a fail, it returns the original 
+    image.
+    
+    Args:
+        image (list) : a 2D nested list representation of an image, where
+                       0 represents an unfilled pixel, and
+                       1 represents a boundary pixel
+        seed_point (tuple) : a 2-element tuple representing the (row, col) 
+                       coordinates of the seed point to start filling
+                       
+    Returns:
+        image (list) : if any validity test fails
+        (None) : if the seed points are valid
+
+    """
+    
+    # Grid range
+    n_rows = len(image)
+    n_cols = len(image[0])
+    
+    # Seed values
+    seed_row = seed_point[0]
+    seed_col = seed_point[1]
+    
+    # First, check integer values
+    seed_row_is_integer = isinstance(seed_row, int)
+    seed_col_is_integer = isinstance(seed_col, int)
+    if not seed_row_is_integer and seed_col_is_integer:
+        print("Starting point is not a tuple of integers.")
+        show_image(image)
+        return image
+    
+    # Second, check we're in the grid
+    seed_row_in_grid = seed_row in list(range(0, n_rows))
+    seed_col_in_grid = seed_col in list(range(0, n_cols))
+    if not seed_row_in_grid and seed_col_in_grid:
+        print("Starting point is outside the grid range.")
+        show_image(image)
+        return image       
+    
+    # Third, check we're on an unfilled pixel
+    if not image[seed_row][seed_col] == 0:
+        print("Starting point is on a boundary.")
+        show_image(image)
+        return image
+ 
+def get_bordering_pixels(image, coords):
+   """ We zoom in on a pixel and get the characteristics of its neigbbours.
+   
+   If the pixel's neighbours (up, down, left, right) would be outside the bounds
+   of the image, return None for its position and value.
+   
+   Args:
+        image (list) : a 2D nested list representation of an image, where
+                       0 represents an unfilled pixel, and
+                       1 represents a boundary pixel
+        coords (tuple) : a 2-element tuple representing the (row, col) 
+                       coordinates of the seed point to start filling
+                       
+    Returns:
+        (dict) : Nested dictionary, keyed by left, up, down, or right.
+                 Pixel properties are its position and its value. If it's
+                 out of bounds it returns None for both of these.
+   
+   """
+   
+   n_rows = len(image)
+   n_cols = len(image[0])
+    
+   row = coords[0]
+   col = coords[1]
+   
+   if col-1 not in list(range(n_cols)):
+       left_pixel = None
+       left_pixel_val = None
+   else:
+       left_pixel = (row, col-1)
+       left_pixel_val = image[row][col-1]
+        
+   if col+1 not in list(range(n_cols)):
+       right_pixel = None
+       right_pixel_val = None
+   else:
+       right_pixel = (row, col+1)
+       right_pixel_val = image[row][col+1]
+        
+   if row-1 not in list(range(n_rows)):
+       up_pixel = None
+       up_pixel_val = None
+   else:
+       up_pixel = (row-1, col)
+       up_pixel_val = image[row-1][col]
+        
+   if row+1 not in list(range(n_rows)):
+       down_pixel = None
+       down_pixel_val = None
+   else:
+       down_pixel = (row+1, col)
+       down_pixel_val = image[row+1][col]
+          
+   return {"left_pixel": {"position": left_pixel, "value": left_pixel_val},
+            "up_pixel": {"position": up_pixel, "value": up_pixel_val},
+            "right_pixel": {"position": right_pixel, "value": right_pixel_val},
+            "down_pixel": {"position": down_pixel, "value": down_pixel_val}} 
+
+
+def fill_surrounding_pixels(image, coords):
+    
+    row = coords[0]
+    col = coords[1]
+    if image[row][col] == 0:
+        image[row][col] = 2
+    
+    adjacent_pixels = get_bordering_pixels(image, coords)
+    
+    for pixel in ["left_pixel", "up_pixel", "right_pixel", "down_pixel"]:     
+        if adjacent_pixels[pixel]["value"] == 0:
+            row = adjacent_pixels[pixel]["position"][0]
+            col = adjacent_pixels[pixel]["position"][1]            
+            image[row][col] = 2
+            fill_surrounding_pixels(coords = (row, col), image = image)
+    return image      
+
+    
 def fill(image, seed_point):
     """ Fill the image from seed point to boundary
 
@@ -102,10 +231,14 @@ def fill(image, seed_point):
                2 represents a filled pixel
     """
 
-    # TODO: Complete this function
-    return []
+    # First, check we have a valid starting point
+    validate_seed_point(image, seed_point)
+    
+    # If so, fill the empty pixels and return the new image
+    image = fill_surrounding_pixels(image, seed_point)
 
-
+    return image
+    
 def example_fill():
     image = load_image("data/bar.txt")
 
@@ -118,7 +251,10 @@ def example_fill():
     print("After filling:")
     show_image(image)
 
-
+"""
 if __name__ == '__main__':
     example_fill()
+"""
 
+
+show_image(fill(load_image("data/snake.txt"), (12,6)))
--- test_bucket_fill.py
+++ test_bucket_fill.py
@@ -1,5 +1,24 @@
 from bucket_fill import fill
 
+def test_true_seeds():  
+    assert (validate_seed_point(image = load_image("data/smiley.txt"), seed_point = (4,4)) is None)
+    assert (validate_seed_point(image = load_image("data/smiley.txt"), seed_point = (0,0)) is None)
+    assert (validate_seed_point(image = load_image("data/smiley.txt"), seed_point = (10,0)) is None)
+    assert (validate_seed_point(image = load_image("data/smiley.txt"), seed_point = (10,10)) is None)
+    
+def test_false_seeds():
+    assert (validate_seed_point(image = load_image("data/smiley.txt"), seed_point = (400,4)) is not None)
+    assert (validate_seed_point(image = load_image("data/smiley.txt"), seed_point = ("Hello",0)) is not None)
+    assert (validate_seed_point(image = load_image("data/smiley.txt"), seed_point = (4.4,6)) is not None)
+    assert (validate_seed_point(image = load_image("data/smiley.txt"), seed_point = (-1,10)) is not None)
+    assert (validate_seed_point(image = load_image("data/smiley.txt"), seed_point = (10,1)) is not None)
+
+test_true_seeds()
+test_false_seeds()
+
+print(get_bordering_pixels(load_image("data/snake.txt"), (2,1)))
+
+
 def test_pattern():
     pass
 
